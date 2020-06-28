349ba120b242d24b7072a4133f22fbc7f7eecd80
Andre Souza <andresouza4@gmail.com>
2020-06-27 17:38:03 -0700

    Add nice songs

--- pages/api/musics.js
+++ pages/api/musics.js
@@ -1,12 +1,12 @@
 export default (req, res) => {
   res.status(200).json([
-    { id: 'one', name: 'One', duration: '2:10' },
-    { id: 'two', name: 'Two', duration: '1:40' },
-    { id: 'three', name: 'Three', duration: '2:15' },
-    { id: 'four', name: 'Four', duration: '2:00' },
-    { id: 'five', name: 'Five', duration: '2:21' },
-    { id: 'six', name: 'Six', duration: '1:55' },
-    { id: 'seven', name: 'Seven', duration: '1:59' },
-    { id: 'eight', name: 'Eight', duration: '2:05' }
+    { id: 'one', name: 'Punch Bag', duration: '2:10' },
+    { id: 'two', name: 'Headlock', duration: '1:40' },
+    { id: 'three', name: 'Legendary', duration: '2:15' },
+    { id: 'four', name: "Can't Stop", duration: '2:00' },
+    { id: 'five', name: 'The Runner', duration: '2:21' },
+    { id: 'six', name: 'Jumpsuit', duration: '1:55' },
+    { id: 'seven', name: 'Glory', duration: '1:59' },
+    { id: 'eight', name: 'Play God', duration: '2:05' }
   ])
 }
